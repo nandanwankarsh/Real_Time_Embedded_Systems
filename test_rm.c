8a4c0acdef336312f62c03ba4676ba152ddd84aa
Shubham Hemantrao Nandanwankar <31578701+nandanwankarsh@users.noreply.github.com>
2018-02-24 15:47:45 -0800

    RM with sort

--- test_rm.c
+++ test_rm.c
@@ -7,27 +7,32 @@
 #include <errno.h>
 #include <math.h>
 
+void sort_rm(float taskset[][3],int no_tasks,int index);
 void del(char str[], char ch);
+void swap(float *a,float *b);
+void wrapper_rm(char* str_taskset,int no_tasks);
 void rm(char* taskset,int no_tasks);
 int utilization_bound(int no_tasks, float* taskset);
 int time_demand(int no_tasks, float* taskset);
 
-void main(){
+
+
+int main(){
 
 	FILE *fp;
 	char line1[30];
 	char *line1_str[2],*str_woeol;
-	int num_lines,no_taskset;
+	int num_lines,num_taskset;
 
 	if ((fp=fopen("input.txt","r")) == NULL)
 		printf("Cannot open file\n");
 
 	fgets(line1, sizeof(line1), fp);
 	del(line1,'\n');
-	printf ("Splitting string \"%s\" into tokens:\n",line1);
-	no_taskset=atoi(line1);
-	// printf("No of taskset %d\n",no_taskset);
-	for(int j = 0; j < no_taskset; j++){
+	// printf ("Splitting string \"%s\" into tokens:\n",line1);
+	num_taskset=atoi(line1);
+	// printf("No of taskset %d\n",num_taskset);
+	for(int j = 0; j < num_taskset; j++){
 		fgets(line1, sizeof(line1), fp);
 		del(line1,'\n');	
 		num_lines=atoi(line1);
@@ -42,10 +47,100 @@ void main(){
 			strcat(holder,line[i]);
 			// printf("%s\n", line[i]);
 		}
-		// printf("%s\n", holder);
 		rm(holder,num_lines);
+		// printf("%s\n", holder);
+		// edf(holder,num_lines);
 		printf("\n");
 	}
+return 0;
+}
+
+
+void swap(float *a,float *b){
+	for(int i=0;i<3;i++){
+		a[i]=a[i]+b[i];
+		b[i]=a[i]-b[i];
+		a[i]=a[i]-b[i];
+
+	}
+
+}
+
+// void wrapper_rm(char* str_taskset,int no_tasks){
+
+// 	float taskset_mat[no_tasks][3],taskset[3*no_tasks];
+// 	char *token; 
+// 	int count=0;   
+// 	token = strtok (str_taskset," ");
+// 	for (int i=0;i<no_tasks;i++){
+// 		for(int j=0;j<3;j++){
+// 			taskset_mat[i][j]=atof(token);
+// 			token = strtok (NULL, " ");
+// 		//	printf("%f \t",taskset[i]);
+// 		}
+// 	}
+// 	sort_rm(taskset_mat,no_tasks,1);
+
+// 	for (int i=0;i<no_tasks;i++){
+// 		for(int j=0;j<3;j++){
+
+// 			taskset[i*3+j]=taskset_mat[i][j];
+// 		}
+// 	}
+
+// 	for(int i=0;i<3*no_tasks;i++){
+
+// 		printf("%f\t",taskset[i]);
+// 	}
+
+// }
+
+
+void sort_rm(float taskset[][3],int no_tasks,int index){
+
+	
+
+
+	for (int i = 0; i < (no_tasks); i = i+1){
+
+		for(int j=0;j< no_tasks;j++){
+
+				// if(taskset[j][index]<taskset[j+1][index])
+				// 	swap(taskset[j],taskset[j+1]);
+
+			printf("%f\t",taskset[i][j]);
+		}
+		 printf("\n");
+	}
+
+	
+	for (int i = 0; i < (no_tasks); i = i+1){
+
+		for(int j=0;j< no_tasks-1;j++){
+
+				if(taskset[j][index]>taskset[j+1][index])
+					swap(taskset[j],taskset[j+1]);
+
+			// printf("%f\t",taskset[i][j]);
+		}
+		// printf("\n");
+	}
+	printf("After sorting\n");
+	for (int i = 0; i < (no_tasks); i = i+1){
+
+		for(int j=0;j< no_tasks;j++){
+
+				// if(taskset[j][index]<taskset[j+1][index])
+				// 	swap(taskset[j],taskset[j+1]);
+
+			printf("%f\t",taskset[i][j]);
+		}
+		 printf("\n");
+	}
+
+
+
+
 }
 
 void del(char str[], char ch) {
@@ -68,18 +163,32 @@ void del(char str[], char ch) {
 	// printf("\ncorrected string is : %s \t %s", str1,str);
 }
 
+
 void rm(char* str_taskset,int no_tasks){
 
-	float taskset[3*no_tasks];
+	float taskset_mat[no_tasks][3],taskset[3*no_tasks];
 	char *token; 
 	int count=0;   
 	token = strtok (str_taskset," ");
-	for (int i=0;i<3*no_tasks;i++){
-		taskset[i]=atof(token);
-		token = strtok (NULL, " ");
-		printf("%f \t",taskset[i]);
+	for (int i=0;i<no_tasks;i++){
+		for(int j=0;j<3;j++){
+			taskset_mat[i][j]=atof(token);
+			token = strtok (NULL, " ");
+		//	printf("%f \t",taskset[i]);
+		}
 	}
-	printf("\n");
+	for (int i=0;i<no_tasks;i++){
+		for(int j=0;j<3;j++){
+
+				taskset[i*3+j]=taskset_mat[i][j];
+			}
+		}
+
+	for(int i=0;i<3*no_tasks;i++){
+
+		printf("%f\t",taskset[i]);
+	}
+
 	for (int i = 0; i < (3*no_tasks); i = i+3){
 
 		if(taskset[i+1] == taskset[i+2]){
@@ -87,20 +196,28 @@ void rm(char* str_taskset,int no_tasks){
 			count++;
 		}
 	}
-
+	printf("%d\n",count);
 	if (count == no_tasks){
 
-	int u = utilization_bound(no_tasks ,taskset);
-	if (u == 0){
-		
-		printf("U is less than 1 so schedulable directly\n");
-	}
-	else if(u == 1){
+		sort_rm(taskset_mat,no_tasks,2);
 
-		printf("U is greater than 1 so not schedulable\n");
-	}
-	else
-		printf("U is greater than utilization_bound and less than 1, so need to do time demand analysis\n");
+		for (int i=0;i<no_tasks;i++){
+			for(int j=0;j<3;j++){
+
+				taskset[i*3+j]=taskset_mat[i][j];
+			}
+		}
+		int u = utilization_bound(no_tasks ,taskset);
+		if (u == 0){
+			
+			printf("U is less than 1 so schedulable directly\n");
+		}
+		else if(u == 1){
+
+			printf("U is greater than 1 so not schedulable\n");
+		}
+		else
+			printf("U is greater than utilization_bound and less than 1, so need to do time demand analysis\n");
 
 		for (int nt = 1; nt <= no_tasks; nt++){
 
@@ -134,7 +251,7 @@ int utilization_bound(int no_tasks, float* taskset){
 	float u=0,bound=0;
 	int ret;
 	for (int i = 0; i < (3*no_tasks); i = i+3){
-		u += (taskset[i]/taskset[i+1]);
+		u += (taskset[i]/taskset[i+2]);
 	}
 
 	bound = no_tasks*(pow(2.0,(1.0/no_tasks)) - 1);
