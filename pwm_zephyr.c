5aa4bf74384c007850a7b84ec8d54dec015f1c92
Shubham Hemantrao Nandanwankar <31578701+nandanwankarsh@users.noreply.github.com>
2018-03-29 00:09:10 -0700

    Updated pwm_zephyr
    Without background computation

--- pwm_zephyr.c
+++ pwm_zephyr.c
@@ -26,62 +26,113 @@ struct device *dev6;
 
 // u32_t start_time;
 long long start_time;
-// u32_t stop_time;
+// // // u32_t stop_time;
 long long stop_time;
+// uint32_t start_time;
+// uint32_t stop_time;
 // u32_t diff;
 long long diff;
 u32_t cycles_spent;
 // u32_t nanoseconds_spent;
 long long milliseconds_spent;
-
+int count=0;
 
 void gpio_set();
 
 void handler(struct device *dev6, struct gpio_callback *callb, u32_t pin){
-	printk("In handler wohoo!!! at\n");
+	stop_time=_tsc_read();
+	// printk("Stop ticks %d \n",stop_time);
+	// printk("In handler wohoo!!! at %d\n",stop_time-start_time);
+	diff=stop_time-start_time;
+	printk("Cycles per pwm %d\n",diff);
+	count+=1;
 }
 
 void main(void)
 {
 	int cnt = 0,flag;
+	int val=0;
 	struct device *pwm_dev;
 	struct device *pinmux;
+	unsigned long long int *c,z;
+	c=&z;
 
 	pwm_dev = device_get_binding(PWM0);
 	pinmux = device_get_binding(MUX);
 	pinmux_pin_set(pinmux, 5, PINMUX_FUNC_C);
 
-	pwm_pin_set_cycles(pwm_dev,3,4095,1000);
+	
 
 	gpio_set();
 
 	printk("Hello World! %s\n", CONFIG_ARCH);
-
-
-
-
-					
-
 	gpio_init_callback(&callb,handler, BIT(3));
 
 	gpio_add_callback(dev6, &callb);
 	gpio_pin_enable_callback(dev6,3);
-	int val=0;
+	// int key=irq_lock();
+	
+	
+	// if(pwm_get_cycles_per_sec(pwm_dev,3,c)!=0)
+	// 	printk("Error pwm cps\n");
+	start_time=k_cycle_get_32();
+	printk("Start ticks %d \n",start_time);
+	pwm_pin_set_cycles(pwm_dev,3,4095,1000);
+
+	while(1){
+		start_time=_tsc_read();
+
+		if(count==50)
+			break;
+
+	}
+	
+	// gpio_remove_callback(dev6,&callb);
+
+	// printk("Stop ticks %d \n",stop_time);
+	// while(count<10){
+	//  while(1){
+	//  	gpio_pin_read(dev6,3, &val);
+	//  	if(val==1){
+	//  		start_time=k_cycle_get_32();
+	//  		break;
+	//  	}
+
+	//  }
+
+	//  while(1){
+	//  		gpio_pin_read(dev6,3, &val);
+ // 			if(val==1){
+	//  			stop_time=k_cycle_get_32();
+	//  			break;
+ // 			}
+
+	//  }
+	//  printk("Cycles per pwm %d\n",stop_time-start_time);
+	//  count++;
+	// }
+
+
+	
+
+
 	// while(1){
 	// 	// int val=0;
 	// 	gpio_pin_read(dev6,3, &val);
 	// 	printk("read value:%d\n",val);
-	// 	k_sleep(1000);
+	
 		
 	// }
-	k_sleep(10000);
-	printk("End\n");
+	// k_sleep(10000);
+	// printk("End\n");
 
-	while (1) {
+	// while (count < 10) {
 
-		//Do nothing
-
-	}
+	// 	k_sleep(3);
+	
+	// }
+	gpio_pin_disable_callback(dev6,3);
+	
 }
 
 void gpio_set(){
